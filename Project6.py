d4b92d0a48f59562f23f93b7d0d5ee75eee60451
mycahdt <79180400+mycahdt@users.noreply.github.com>
2023-08-03 13:33:11 -0400

    Add files via upload

---
+++ Project6.py
@@ -0,0 +1,14 @@
+#Project 6
+'''
+Create survey question that is creative and unique;
+Answer should be non-nominal number;
+At least 16 people answer the question
+'''
+
+QUESTION = "How many reusable face masks do you own?"
+
+ANSWERS = [3, 5, 3, 5, 8, 20, 7, 3, 7, 3, 2, 0, 10, 3, 4, 2, 10, 5, 7, 10, 3, 2, 6]
+
+EMAIL = "mycahdt@udel.edu"
+
+PLEDGE = "I swear that I actually asked this question of at least 16 people and recorded their data without modifying the results. I understand that falsifying data in this course is really lame."
